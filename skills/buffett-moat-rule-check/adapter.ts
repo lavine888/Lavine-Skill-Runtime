288636e06ebcd1838b9f8915ba7b6c43b274da7e
LavineX <dbest72500@gmail.com>
2026-09-02 16:19:25 +0800

    feat: add Buffett Python adapter

---
+++ skills/buffett-moat-rule-check/adapter.ts
@@ -0,0 +1,6 @@
+import type { PythonSkillAdapter } from "../../runtime/types";
+
+export const buffettMoatRuleCheckAdapter: PythonSkillAdapter = {
+  id: "buffett-moat-rule-check",
+  runtime: "python",
+};
